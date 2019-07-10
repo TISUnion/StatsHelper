3f7510b8c2eba438f9a66751519a279d7d101d9b
Fallen_Breath <yqx791125833@gmail.com>
2019-07-10 20:15:42 +0800

    complete help msg 3rd

--- StatsHelper.py
+++ StatsHelper.py
@@ -16,9 +16,9 @@ help_msg = '''------MCD StatsHelper插件 v1.2------
 §7''' + prefix + ''' rank §6[统计类别] §e[统计内容] §7(-bot)§r §7(-tell)§r
 §a【参数说明】§r
 §6[统计类别]§r: §6killed§r, §6killed_by§r, §6dropped§r, §6picked_up§r, §6used§r, §6mined§r, §6broken§r, §6crafted§r, §6custom§r
-§6killed§r, §6killed_by§r 的§e[统计内容]§r为§e[生物id]§r
-§6picked_up§r, §6used§r, §6mined§r, §6broken§r, §6crafted§r 的§e[统计内容]为§e[物品/方块id]§r
-§6custom§r 的§e[统计内容]§r详见统计信息的json文件
+§6killed§r, §6killed_by§r 的 §e[统计内容] §r为 §e[生物id]§r
+§6picked_up§r, §6used§r, §6mined§r, §6broken§r, §6crafted§r 的 §e[统计内容]§r 为 §e[物品/方块id]§r
+§6custom§r 的 §e[统计内容]§r 详见统计信息的json文件
 上述内容无需带minecraft前缀
 §7(-uuid)§r: 用uuid替换玩家名; §7(-bot)§r: 统计bot与cam; §7(-tell)§r: 仅自己可见
 §a【例子】§r
