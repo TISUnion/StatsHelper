9e55ef314d772c6c50842adc7b1caa0a7db8e99a
Fallen_Breath <yqx791125833@gmail.com>
2019-11-02 17:46:26 +0800

    v3.0: add uuid cache and -all option

--- StatsHelper.py
+++ StatsHelper.py
@@ -9,9 +9,11 @@ ServerPath = 'server/'
 WorldPath = ServerPath + 'world/'
 Prefix = '!!stats'
 ScoreboardName = 'StatsHelper'
+UUIDFile = 'uuid.json'
 DebugOutput = 0
 RankAmount = 15
-HelpMessage = '''------MCD StatsHelper插件 v2.0------
+rankColor = ['§b', '§d', '§e', '§f']
+HelpMessage = '''------MCD StatsHelper插件 v3.0------
 一个统计信息助手插件，可查询/排名/使用计分板列出各类统计信息。
 §a【格式说明】§r
 §7''' + Prefix + '''§r 显示帮助信息
@@ -20,18 +22,21 @@ HelpMessage = '''------MCD StatsHelper插件 v2.0------
 §7''' + Prefix + ''' scoreboard §6[统计类别] §e[统计内容] §7(-bot)§r
 §7''' + Prefix + ''' scoreboard show§r 显示该插件的计分板
 §7''' + Prefix + ''' scoreboard hide§r 隐藏该插件的计分板
+§7''' + Prefix + ''' refreshUUID§r 刷新玩家UUID列表，插件重载后使用
 §a【参数说明】§r
 §6[统计类别]§r: §6killed§r, §6killed_by§r, §6dropped§r, §6picked_up§r, §6used§r, §6mined§r, §6broken§r, §6crafted§r, §6custom§r
 §6killed§r, §6killed_by§r 的 §e[统计内容] §r为 §e[生物id]§r
 §6picked_up§r, §6used§r, §6mined§r, §6broken§r, §6crafted§r 的 §e[统计内容]§r 为 §e[物品/方块id]§r
 §6custom§r 的 §e[统计内容]§r 详见统计信息的json文件
 上述内容无需带minecraft前缀
-§7(-uuid)§r: 用uuid替换玩家名; §7(-bot)§r: 统计bot与cam; §7(-tell)§r: 仅自己可见
+§7(-uuid)§r: 用uuid替换玩家名; §7(-bot)§r: 统计bot与cam; §7(-tell)§r: 仅自己可见; §7(-all)§r: 列出所有项
 §a【例子】§r
 §7''' + Prefix + ''' query §bFallen_Breath §6used §ewater_bucket§r
 §7''' + Prefix + ''' rank §6custom §etime_since_rest §7-bot§r
 §7''' + Prefix + ''' scoreboard §6mined §estone§r
 '''
+
+UUID = {}
 
 def DebugPrint(server, info, msg):
 	if DebugOutput:
@@ -51,6 +56,19 @@ def name_to_uuid_fromAPI(name):
 	return js['offlinesplitteduuid']
 	
 def name_to_uuid(server, info, name):
+	global UUID
+	if UUID.has_key(name):
+		return UUID[name]
+	printMessage(server, info, 'name not found, use API')
+	UUID[name] = name_to_uuid_fromAPI(name)
+	return UUID[name]
+
+def refreshUUIDList(server, showTip = False):
+	global UUID
+	UUID_file = {}
+	UUID_cache = {}
+	if os.path.isfile(UUIDFile):
+		UUID_file = json.load(open(UUIDFile, 'r'))
 	fileName = ServerPath + 'usercache.json'
 	if os.path.isfile(fileName):
 		with open(fileName, 'r') as f:
@@ -60,11 +78,12 @@ def name_to_uuid(server, info, name):
 				printMessage(server, info, 'cann\'t open json file '+fileName)
 				return name_to_uuid_fromAPI(name)
 			for i in js:
-				if i['name'] == name:
-					return i['uuid']
-	printMessage(server, info, 'name not found, use API')
-	return name_to_uuid_fromAPI(name)
-
+				UUID_cache[i['name']] = i['uuid']
+	UUID = dict(UUID, **dict(UUID_cache, **UUID_file))
+	json.dump(UUID, open(UUIDFile, 'w'))
+	if showTip:
+		server.say('UUID列表刷新完成，列表长度为' + str(len(UUID)))
+	
 def isBot(name):
 	blacklist = 'A_Pi#nw#sw#SE#ne#nf#SandWall#storage#zi_ming#Steve#Alex###########'
 	blackkey = ['farm', 'bot_', 'cam', '_b_', 'bot-']
@@ -85,7 +104,7 @@ def printMessage(server, info, msg, isTell = True):
 		else:
 			print line
 			
-def getData(server, info, uuid, classification, target):
+def getStatsData(server, info, uuid, classification, target):
 	jsonfile = WorldPath + 'stats/' + uuid + '.json'
 	if not os.path.isfile(jsonfile):
 		#printMessage(server, info, '未找到该玩家的统计文件！')
@@ -106,29 +125,19 @@ def getData(server, info, uuid, classification, target):
 		return (data, True)
 		
 def getPlayerList(server, info, listBot):
-	fileName = ServerPath + 'usercache.json'
+	global UUID
 	ret = []
-	flag = False
-	if os.path.isfile(fileName):
-		with open(fileName, 'r') as f:
-			try:
-				js = json.load(f)
-			except ValueError:
-				#printMessage(server, info, 'cann\'t open json file ' + fileName)
-				return (ret, False)
-			for i in js:
-				name = i['name']
-				if not listBot and isBot(name):
-					continue
-				ret.append((name, i['uuid']))
-		flag = True
-	else:
-		printMessage(server, info, 'usercache.json not found')
-	return (ret, flag)
+	for i in UUID.items():
+		if listBot or not isBot(i[0]):
+			ret.append(i)
+	return ret
 	
 def triggerSaveAll(server):
 	server.execute('save-all')
 	time.sleep(0.2)
+	
+def getString(classification, target):
+	return '§6' + classification + '§r.§e' + target + '§r'
 
 def showStats(server, info, name, classification, target, isUUID, isTell):
 	uuid = name
@@ -136,38 +145,40 @@ def showStats(server, info, name, classification, target, isUUID, isTell):
 		uuid = name_to_uuid(server, info, uuid)
 	DebugPrint(server, info, 'uuid = ' + uuid)
 	
-	data = getData(server, info, uuid, classification, target)
+	data = getStatsData(server, info, uuid, classification, target)
 	
-	msg = '玩家§b' + name + '§r的统计信息[§6' + classification + '§r.§e' + target + '§r]的值为§a' + str(data) + '§r'
+	msg = '玩家§b' + name + '§r的统计信息[' + getString(classification, target) + ']的值为§a' + str(data) + '§r'
 	printMessage(server, info, msg, isTell)
 		
-def showRank(server, info, classification, target, listBot, isTell):
+def showRank(server, info, classification, target, listBot, isTell, isAll):
 	getPlayerListResult = getPlayerList(server, info, listBot)
-	if getPlayerListResult[1]:
-		arr = []
-		for player in getPlayerListResult[0]:
-			ret = getData(server, info, player[1], classification, target)
-			if ret[1] and ret[0] >= 0:
-				data = ret[0]
-				if DebugOutput > 1: DebugPrint(server, info, 'append [' + name + ', ' + str(data) + ']')
-				arr.append((player[0], data))
+	arr = []
+	sum = 0
+	for player in getPlayerListResult:
+		ret = getStatsData(server, info, player[1], classification, target)
+		if ret[1] and ret[0] > 0:
+			data = ret[0]
+			if DebugOutput > 1: DebugPrint(server, info, 'append [' + name + ', ' + str(data) + ']')
+			arr.append((player[0], data))
+			sum += data
+
+	if len(arr) == 0:
+		printMessage(server, info, '未找到该统计项或该统计项全空！')
+		return
+	arr.sort(key = lambda x:x[0])
+	arr.reverse()
+	arr.sort(key = lambda x:x[1])
+	arr.reverse()
 	
-		if len(arr) == 0:
-			printMessage(server, info, '未找到该统计项或该统计项全空！')
-			return
-		arr.sort(key = lambda x:x[0])
-		arr.reverse()
-		arr.sort(key = lambda x:x[1])
-		arr.reverse()
-		
-		printMessage(server, info, '统计信息[§6' + classification + '§r.§e' + target + '§r]的前十五名为', isTell)
-		maxnamelen = 0
-		for i in range(0, min(RankAmount, len(arr))):
-			maxnamelen = max(maxnamelen, len(str(arr[i][1])))
-		for i in range(0, min(RankAmount, len(arr))):
-			printMessage(server, info, '#' + str(i + 1) + ' ' * (3-len(str(i + 1))) + str(arr[i][1]) + ' ' * (maxnamelen - len(str(arr[i][1])) + 1) + arr[i][0], isTell)
-	else:
-		printMessage(server, info, '玩家列表读取失败')
+	showRange = min(RankAmount + isAll * len(arr), len(arr))
+	printMessage(server, info, '统计信息[' + getString(classification, target) + ']的总数为§c' + str(sum) + '§r，前' + str(showRange) + '名为', isTell)
+	maxNameLength = 0
+	for i in range(0, showRange):
+		maxNameLength = max(maxNameLength, len(str(arr[i][1])))
+	for i in range(0, showRange):
+		printMessage(server, info, rankColor[min(i, len(rankColor) - 1)] + '#' + str(i + 1) + ' ' * (4-len(str(i + 1))) 
+			+ str(arr[i][1]) + ' ' * (maxNameLength - len(str(arr[i][1])) + 2)
+			+ str(arr[i][0]), isTell)
 
 def showScoreboard(server, info):
 	server.execute('scoreboard objectives setdisplay sidebar ' + ScoreboardName)
@@ -184,7 +195,7 @@ def buildScoreboard(server, info, classification, target, listBot):
 			'minecraft.' + classification + ':minecraft.' + target + 
 			' {"text":"§6' + classification + '§r.§e' + target + '"}')
 		for player in getPlayerListResult[0]:
-			ret = getData(server, info, player[1], classification, target)
+			ret = getStatsData(server, info, player[1], classification, target)
 			if ret[1]:
 				server.execute('scoreboard players set ' + player[0] + ' ' + ScoreboardName + ' ' + str(ret[0]))
 		showScoreboard(server, info)
@@ -199,6 +210,8 @@ def onServerInfo(server, info):
 	content = content.replace('-bot', '')
 	isTell = content.find('-tell') >= 0
 	content = content.replace('-tell', '')
+	isAll = content.find('-all') >= 0
+	content = content.replace('-all', '')
 	if not info.isPlayer and content.endswith('<--[HERE]'):
 		content = content.replace('<--[HERE]', '')
 		
@@ -213,7 +226,6 @@ def onServerInfo(server, info):
 		printMessage(server, info, HelpMessage)
 		return
 	
-	
 	cmdlen = len(command)
 	DebugPrintList(server, info, 'processed command = ', command)
 	DebugPrint(server, info, '[isUUID, listBot] = [' + str(isUUID) + ', ' + str(listBot) + ']')
@@ -222,12 +234,20 @@ def onServerInfo(server, info):
 		showStats(server, info, command[1], command[2], command[3], isUUID, isTell)
 	# rank [统计类别] [统计内容] (过滤bot前缀)
 	elif cmdlen == 3 and command[0] == 'rank':
-		showRank(server, info, command[1], command[2], listBot, isTell)
+		showRank(server, info, command[1], command[2], listBot, isTell, isAll)
 	elif cmdlen == 3 and command[0] == 'scoreboard':
 		buildScoreboard(server, info, command[1], command[2], listBot)
 	elif cmdlen == 2 and command[0] == 'scoreboard' and command[1] == 'show':
 		showScoreboard(server, info)
 	elif cmdlen == 2 and command[0] == 'scoreboard' and command[1] == 'hide':
 		hideScoreboard(server, info)
+	elif cmdlen == 1 and command[0] == 'refreshUUID':
+		refreshUUIDList(server, True);
 	else:
 		printMessage(server, info, '参数错误！请输入'+Prefix+'以获取插件帮助')
+
+def onServerStartup(server):
+	refreshUUIDList(server)
+	
+def onPlayerJoin(server, playername):
+	refreshUUIDList(server)
