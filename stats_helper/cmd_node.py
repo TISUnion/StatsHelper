df4bb01d8e0242acf8a4188f4bab403e0381992b
Fallen_Breath <yqx791125833@gmail.com>
2021-08-14 18:25:26 +0800

    MCDR 2.0 update and command tree yes

---
+++ stats_helper/cmd_node.py
@@ -0,0 +1,74 @@
+from mcdreforged.api.command import *
+
+from stats_helper import quick_scoreboard
+
+stored = quick_scoreboard.stored
+
+
+class Arguments:
+	def __init__(self, content: str, total_length: int):
+		self.__arg_set = set(content.split(' '))
+		for arg in self.__arg_set:
+			if len(arg) > 0 and not arg.startswith('-'):
+				raise CommandSyntaxError('Non-arg value "{}" found'.format(content), total_length)
+
+	@classmethod
+	def empty(cls):
+		return Arguments('', 0)
+
+	@property
+	def is_uuid(self) -> bool:
+		return '-uuid' in self.__arg_set
+
+	@property
+	def is_bot(self) -> bool:
+		return '-bot' in self.__arg_set
+
+	@property
+	def is_tell(self) -> bool:
+		return '-tell' in self.__arg_set
+
+	@property
+	def is_all(self) -> bool:
+		return '-all' in self.__arg_set
+
+
+class ArgumentEnding(ArgumentNode):
+	def parse(self, text: str) -> ParseResult:
+		return ParseResult(Arguments(text, len(text)), len(text))
+
+
+class NameAndArgumentEnding(QuotableText):
+	def parse(self, text: str) -> ParseResult:
+		parse_name = super(NameAndArgumentEnding, self).parse(text)
+		name = parse_name.value
+		value_name = None
+		if not name.startswith('-'):
+			value_name = name
+			text_for_arguments = text[parse_name.char_read:]
+		else:
+			text_for_arguments = text
+		return ParseResult((value_name, Arguments(text_for_arguments, len(text))), len(text))
+
+
+class UnknownQuickScoreboard(IllegalArgument):
+	def __init__(self, message: str, char_read: int, alias: str):
+		super().__init__(message, char_read)
+		self.alias = alias
+
+	def get_error_data(self) -> tuple:
+		return (self.alias, )
+
+
+class ScoreboardQuery(ArgumentNode):
+	def parse(self, text: str) -> ParseResult:
+		arg1 = command_builder_util.get_element(text)
+		scoreboard = stored.get(arg1)
+		if scoreboard is not None:
+			return ParseResult(scoreboard, len(arg1))  # <alias>
+		remaining = command_builder_util.remove_divider_prefix(text[len(arg1):])
+		arg2 = command_builder_util.get_element(remaining)
+		if len(arg2) > 0 and arg2[0].isalpha():
+			return ParseResult((arg1, arg2), len(text) - len(remaining) + len(arg2))  # <cls> <target>
+		else:
+			raise UnknownQuickScoreboard('Unknown scoreboard {}'.format(arg1), len(arg1), arg1)
---
+++ stats_helper/constants.py
@@ -0,0 +1,12 @@
+import os
+
+ServerPath = 'server'
+WorldPath = os.path.join(ServerPath, 'world')
+Prefix = '!!stats'
+PluginName = 'StatsHelper'
+ScoreboardName = PluginName
+UUIDFile = os.path.join('config', PluginName, 'uuid.json')
+UUIDFilePrev = os.path.join('plugins', PluginName, 'uuid.json')
+QuickScoreboardFile = os.path.join('config', PluginName, 'quick_scoreboard.json')
+RankAmount = 15
+rankColor = ['§b', '§d', '§e', '§f']
